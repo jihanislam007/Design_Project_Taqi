8b7fed9d4a170b486c48fc20c71355034a216763
jihan <jihanislam007@gmail.com>
2019-11-26 21:53:02 +0600

    worker order complete..

---
+++ app/src/main/java/devsbox/com/jihanislam007/designprojecttaqi/WorkerOrderActivity.java
@@ -0,0 +1,14 @@
+package devsbox.com.jihanislam007.designprojecttaqi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkerOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_order);
+    }
+}
